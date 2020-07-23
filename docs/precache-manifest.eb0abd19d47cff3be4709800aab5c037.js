a455227df7b35a934858a36930c2857c1179dfb2
Álvaro <desarroceveingtcin@gmail.com>
2020-07-22 20:59:18 -0400

    docs agregado

---
+++ docs/precache-manifest.eb0abd19d47cff3be4709800aab5c037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd952e05de7f900035f376441b4315e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "564b4a93215595f16663",
+    "url": "/static/css/main.0e5b3d97.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564b4a93215595f16663",
+    "url": "/static/js/main.293d4515.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
